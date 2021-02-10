8606329e28f19a1b0df834c7a40dafc2d42629a7
steveortizpacheco <38694783+steveortizpacheco@users.noreply.github.com>
2021-02-09 22:22:10 -0500

    crear url
    subir proyecto para ver el flujo navegacion

---
+++ js/formulario.js
@@ -0,0 +1,47 @@
+/*function mostrar () {
+      document.getElementById('mostrarOcultar').style.display="none";
+    
+
+    function ocultar () {
+      document.getElementById('mostrarOcultar').style.display="block";*/
+    
+
+ /*$(document).on("ready", main;
+  function main(){
+	$("#mostrarOcultar").on("click", abrirCerrar;
+}
+ 	
+ 	function abrirCerrar(){
+ 		if($(this).hasClass("abrir")){
+ 			$(this).removeClass("abrir");
+ 		}
+ 		else{
+ 			$(this).addClass("abrir")
+ 		}
+
+}*/
+
+
+ 	const abrircerrar = document.getElementById('abrircerrar');
+    const removeabrircerrar = document.getElementById('removeabrircerrar');
+    const text = document.getElementById('text');
+    abrircerrar.addEventListener('click', function () {
+    	text.classList.toggle('bold');
+    	
+
+    	
+    	
+    });
+    /*removeabrircerrar.addEventListener('click', function () {
+    	text.classList.toggle('normal');
+
+    	});*/
+    
+
+    /*$(document).on("ready", function(){
+    	$(document).on("click", function(){
+    		$("#off_contenido").removeClass("off_contenido")
+
+    	});
+
+    });*/
